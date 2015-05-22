5fb6011970e0e74c2e5e55a7dcbb5ffa43886965
gaojiexuan <gaojiexuan@baidu.com>
2015-05-22 08:16:03 +0800

    ci test

---
+++ test/unit/rosetta_test.js
@@ -0,0 +1,13 @@
+var Rosetta = require('../../lib/rosetta.js');
+var test = require('tape');
+
+
+// test('', function(t) {
+//     t.plan(1);
+
+//     Rosetta.register('r-a', function(tag) {
+
+//     });
+
+//     t.equal();
+// });
---
+++ test/unit/utils_test.js
@@ -0,0 +1,48 @@
+var utils = require('../../lib/utils.js');
+var test = require('tape');
+
+
+test('utils objToString test', function(t) {
+    t.plan(5);
+
+    var rjson = utils.objToString({
+            a: 'sdsd',
+            b: [1, 2, 4, {b: 'dd', c:[3, 2, 4]}]
+        });
+
+    t.equal(rjson, "{a:'sdsd',b:[1,2,4,{b:'dd',c:[3,2,4]}]}");
+
+    var rarray = utils.objToString([{
+            a:"sdfsd",
+            b:[1,2,3,4,{c:2}]
+        }, "ddd", {"dfdf":{bbbb:"dfd"}}])
+
+    t.equal(rarray, '[{a:\'sdfsd\',b:[1,2,3,4,{c:2}]},\'ddd\',{dfdf:{bbbb:\'dfd\'}}]');
+
+
+    var rstring = utils.objToString('dfsasdfs');
+    t.equal(rstring, '\'dfsasdfs\'');
+
+
+    var rfunc = utils.objToString(function(){alert(1)});
+    t.equal(rfunc, 'function (){alert(1)}');
+
+
+    var rspe = utils.objToString('&nbsp');
+    t.equal(rspe, '\'&nbsp\'');
+});
+
+
+test('utils toType test', function(t) {
+    t.plan(2);
+
+    var rjson = utils.toType('{"a":"sdsd","b":[1,2,4,{"b":"dd","c":[3,2,4]}]}');
+
+    t.deepEqual(rjson, { a: 'sdsd', b: [ 1, 2, 4, { b: 'dd', c: [ 3, 2, 4 ] } ] });
+
+
+    var rarray = utils.toType('[{"a":"sdfsd", "b":[1,2,3,4,{"c":2}]}, "ddd", {"dfdf":{"bbbb":"dfd"}}]');
+
+    t.deepEqual(rarray, [{"a":"sdfsd", "b":[1,2,3,4,{"c":2}]}, "ddd", {"dfdf":{"bbbb":"dfd"}}]);
+
+});
