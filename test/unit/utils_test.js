c5bd87a9f3cfbf209c13f2443cc680ab0918d9c8
jiexuangao <jiexuangao@gmail.com>
2015-06-18 07:27:55 +0800

    update

--- test/unit/utils_test.js
+++ test/unit/utils_test.js
@@ -1,53 +1,7 @@
 var utils = require('../../lib/utils.js');
 var test = require('tape');
-require('../../lib/ie-shim.js');
-require('../../lib/es5-shim.js');
+require('../../lib/shims.js');
 
-
-test('utils objToString test', function(t) {
-    t.plan(5);
-
-    var rjson = utils.objToString({
-            a: 'sdsd',
-            b: [1, 2, 4, {b: 'dd', c:[3, 2, 4]}]
-        });
-
-    t.equal(rjson, "{a:'sdsd',b:[1,2,4,{b:'dd',c:[3,2,4]}]}");
-
-    var rarray = utils.objToString([{
-            a:"sdfsd",
-            b:[1,2,3,4,{c:2}]
-        }, "ddd", {"dfdf":{bbbb:"dfd"}}])
-
-    t.equal(rarray, '[{a:\'sdfsd\',b:[1,2,3,4,{c:2}]},\'ddd\',{dfdf:{bbbb:\'dfd\'}}]');
-
-
-    var rstring = utils.objToString('dfsasdfs');
-    t.equal(rstring, '\'dfsasdfs\'');
-
-
-    var rfunc = utils.objToString(function(){alert(1);});
-    t.equal(typeof rfunc, 'string');
-
-
-    var rspe = utils.objToString('&nbsp');
-    t.equal(rspe, '\'&nbsp\'');
-});
-
-
-test('utils toType test', function(t) {
-    t.plan(2);
-
-    var rjson = utils.toType('{"a":"sdsd","b":[1,2,4,{"b":"dd","c":[3,2,4]}]}');
-
-    t.deepEqual(rjson, { a: 'sdsd', b: [ 1, 2, 4, { b: 'dd', c: [ 3, 2, 4 ] } ] });
-
-
-    var rarray = utils.toType('[{"a":"sdfsd", "b":[1,2,3,4,{"c":2}]}, "ddd", {"dfdf":{"bbbb":"dfd"}}]');
-
-    t.deepEqual(rarray, [{"a":"sdfsd", "b":[1,2,3,4,{"c":2}]}, "ddd", {"dfdf":{"bbbb":"dfd"}}]);
-
-});
 
 test('to plain array test', function(t) {
     t.plan(2);
